ae6c26e24b39e603b938b90467f9b8fda32d7d43
DanielPalacios05 <drpalaciod@eafit.edu.co>
2023-09-30 23:05:07 -0500

    testing udp

---
+++ test/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+
+int main(){
+
+    int i = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    
+
+    return 0;
+}
---
+++ timeserver/client.py
@@ -0,0 +1,15 @@
+# echo-client.py
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 8080  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+
+    while True:
+        a = str.encode(input("say sum: "))
+        s.sendto(a,(HOST,PORT))
+
+        
+    
