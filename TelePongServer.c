0f2455d2e2cf32e0d0e8ab00c4785d0022727baa
DanielPalacios05 <drpalaciod@eafit.edu.co>
2023-10-08 20:50:14 -0500

    base of the project

--- TelePongServer.c
+++
@@ -1,1 +0,0 @@
-#include "myPongProtocol.h"
--- test/networking.h
+++
@@ -1,7 +0,0 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <errno.h>
