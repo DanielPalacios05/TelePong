1b97c5d3af9a60392279d510acf81633803d2ca0
EsteTruji <estebantrujillocarmona@gmail.com>
2023-10-09 14:11:48 -0500

    Cambio en archivo importado

--- TelePongServer.c
+++ TelePongServer.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include "myPongProtocol.c"
 
 #define MAX_PLAYERS 20
 
@@ -28,37 +29,8 @@ struct Game {
 };
 
 int main() {
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_socktype = SOCK_DGRAM;
-
-    struct addrinfo *networkInfo;
-
-    getaddrinfo(0, "8080", &hints, &networkInfo);
-
-    char server_address_buffer[100];
-    getnameinfo(networkInfo->ai_addr, networkInfo->ai_addrlen, server_address_buffer, sizeof(server_address_buffer), 0, 0, NI_NUMERICHOST);
-
-    printf("Server address: %s\n", server_address_buffer);
-
-    int sock = socket(networkInfo->ai_family, networkInfo->ai_socktype, networkInfo->ai_protocol);
-
-    if (sock < 0) {
-        printf("Error creating socket\n");
-        return 1;
-    }
-
-    printf("Binding socket\n");
-
-    if (bind(sock, networkInfo->ai_addr, networkInfo->ai_addrlen)) {
-        printf("Binding failed\n");
-        perror("bind");
-        return 1;
-    }
-
-    freeaddrinfo(networkInfo);
+    
+    int server_socket = createServerSocket();
 
     printf("Waiting for players to connect...\n");
 
@@ -72,7 +44,7 @@ int main() {
     socklen_t client_len = sizeof(client_address);
     char read_data[1024];
 
-    int bytes_read = recvfrom(sock, read_data, sizeof(read_data), 0, (struct sockaddr*)&client_address, &client_len);
+    int bytes_read = recvfrom(server_socket, read_data, sizeof(read_data), 0, (struct sockaddr*)&client_address, &client_len);
 
     int playerIndex = -1;
     for (int i = 0; i < numPlayers; ++i) {
@@ -91,12 +63,12 @@ int main() {
 
     // Send player number (1 or 2) to the client
     char playerNumber = playerIndex + 1;
-    sendto(sock, &playerNumber, sizeof(playerNumber), 0, (struct sockaddr*)&players[playerIndex].address, players[playerIndex].address_len);
+    sendto(server_socket, &playerNumber, sizeof(playerNumber), 0, (struct sockaddr*)&players[playerIndex].address, players[playerIndex].address_len);
 
     printf("Player %d connected.\n", playerIndex + 1);
 
     // Forward the opponent's movement to the player
-    sendto(sock, read_data, bytes_read, 0, (struct sockaddr*)&players[1 - playerIndex].address, players[1 - playerIndex].address_len);
+    sendto(server_socket, read_data, bytes_read, 0, (struct sockaddr*)&players[1 - playerIndex].address, players[1 - playerIndex].address_len);
 
     memset(read_data, 0, sizeof(read_data));
     goto fr;
