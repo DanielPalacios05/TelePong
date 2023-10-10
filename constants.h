7a479033af12ca22bd14b37c383ec8198610886e
EsteTruji <estebantrujillocarmona@gmail.com>
2023-10-09 19:39:08 -0500

    Adición de los struct Player y Game en constants.h

--- constants.h
+++ constants.h
@@ -1,5 +1,23 @@
 #ifndef CONSTANTS_H  // Evita la inclusión múltiple
 #define CONSTANTS_H
+#include <sys/socket.h>
+
+struct Player {
+    struct sockaddr_storage address;
+    socklen_t address_len;
+    char nickname[15];
+    int playerNum;
+    int playerPos;
+};
+
+struct Game {
+    char gameId;
+    int gamePos;
+    struct Player player1;
+    struct Player player2;
+    int p1Score;
+    int p2Score;
+};
 
 #define MAX_PLAYERS 20
 #define PORT 8080
