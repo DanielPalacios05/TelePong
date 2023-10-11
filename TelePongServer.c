a9d82bcc262263687b34eb052808eecd87c01b39
EsteTruji <estebantrujillocarmona@gmail.com>
2023-10-10 21:04:53 -0500

    Ajustes y refinaciones

--- TelePongServer.c
+++ TelePongServer.c
@@ -1,5 +1,4 @@
 #include <sys/types.h>
-
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
@@ -10,43 +9,48 @@
 #include "myPongProtocol.c"
 #include "constants.h"
 
-int main() {
+int main()
+{
 
     int server_socket = createServerSocket();
 
     printf("Waiting for players to connect...\n");
 
-    struct Player players[MAX_PLAYERS];
-    struct Game games[MAX_PLAYERS/2];
     int numPlayers = 0;
     int gamePos = 0;
 
-    for (int i = 0; i < (MAX_PLAYERS/2); i++) {
+    for (int i = 0; i < (MAX_PLAYERS / 2); i++)
+    {
         games[i].totalNumPlayers = 0;
     }
 
-    fr:
-    if (numPlayers < MAX_PLAYERS){
+fr:
+    if (numPlayers < MAX_PLAYERS)
+    {
         players[numPlayers] = receivePlayer(server_socket);
-        
+
         printf("Player rec: %s || ", players[numPlayers].nickname);
 
-        for (int i = 0; i < (MAX_PLAYERS/2); i++) {
+        for (int i = 0; i < (MAX_PLAYERS / 2); i++)
+        {
             printf("Paso por for en %d || ", i);
-            printf("%d",games[i].totalNumPlayers);
-            if(games[i].totalNumPlayers == 0){
+            printf("%d", games[i].totalNumPlayers);
+            if (games[i].totalNumPlayers == 0)
+            {
                 players[numPlayers].playerNum = 1;
                 players[numPlayers].playerPos = numPlayers;
-                players[numPlayers].gameId = i+100;
-                games[i].gameId = i+100;
+                players[numPlayers].gameId = i + 100;
+                games[i].gameId = i + 100;
                 games[i].gamePos = i;
                 games[i].player1 = players[numPlayers];
                 games[i].totalNumPlayers++;
                 break;
-            }else if(games[i].totalNumPlayers == 1){
+            }
+            else if (games[i].totalNumPlayers == 1)
+            {
                 players[numPlayers].playerNum = 2;
                 players[numPlayers].playerPos = numPlayers;
-                players[numPlayers].gameId = i+100;
+                players[numPlayers].gameId = i + 100;
                 games[i].player2 = players[numPlayers];
                 games[i].totalNumPlayers++;
                 gamePos = i;
@@ -56,14 +60,14 @@ int main() {
 
         sendGameInfo(server_socket, players[numPlayers]);
         printf("Gamepos ... %d || ", gamePos);
-        if(players[numPlayers].playerNum == 2){
+        if (players[numPlayers].playerNum == 2)
+        {
             sendOpponent(server_socket, games[gamePos].player2.nickname, games[gamePos].player1);
             sendOpponent(server_socket, games[gamePos].player1.nickname, games[gamePos].player2);
         }
-        
+
         printf("Player %s connected.\n", players[numPlayers].nickname);
         ++numPlayers;
-
     }
     goto fr;
 }
