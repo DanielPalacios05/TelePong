35c13ce9a0fed6bbd5aab9c5a6e4474910021fb5
EsteTruji <estebantrujillocarmona@gmail.com>
2023-10-13 20:41:05 -0500

    Ajustes para escuchar mensajes del cliente con llamado a protocolo

--- server/TelePongServer.c
+++ server/TelePongServer.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include "utils.c"
 #include "myPongProtocol.c"
 #include "constants.h"
+#include "utils.c"
 
 int main()
 {
@@ -18,6 +18,7 @@ int main()
     char server_socketStr[20]; 
     snprintf(server_socketStr, sizeof(server_socketStr), "%d", server_socket);
     response.server_socket = 0;
+    char message[100];
 
     printf("Waiting for players to connect...\n");
 
@@ -30,71 +31,81 @@ int main()
     }
 
 fr:
-    if (numPlayers < MAX_PLAYERS)
-    {
-        memset(&response.player, 0, sizeof(struct Player)); // Asigna ceros a la estructura Player
-        char message[100];
-        snprintf(message, sizeof(message), "PLAYER RECEIVE %s", server_socketStr);
-        response = handleCommunication(message);
-        players[numPlayers] = response.player;
-
-        printf("Player rec: %s || ", players[numPlayers].nickname);
-
-        for (int i = 0; i < (MAX_PLAYERS / 2); i++)
+    strcpy(message, "SERVER LISTEN_MSG ");
+    concat(message, 1, server_socketStr);
+    response = handleCommunication(message);
+    
+    if(strcmp(response.msg, "SERVER INIT_PLAYER") == 0){
+        printf("Mensaje ::::  %s \n", response.msg);
+        if (numPlayers < MAX_PLAYERS)
         {
-            printf("Paso por for en %d || ", i);
-            printf("%d", games[i].totalNumPlayers);
-            if (games[i].totalNumPlayers == 0)
+            memset(&response.player, 0, sizeof(struct Player)); // Asigna ceros a la estructura Player
+            snprintf(message, sizeof(message), "PLAYER RECEIVE %s", server_socketStr);
+            response = handleCommunication(message);
+            players[numPlayers] = response.player;
+
+            printf("Player rec: %s || ", players[numPlayers].nickname);
+
+            for (int i = 0; i < (MAX_PLAYERS / 2); i++)
             {
-                players[numPlayers].playerNum = 1;
-                players[numPlayers].playerPos = numPlayers;
-                players[numPlayers].gameId = i + 100;
-                games[i].gameId = i + 100;
-                games[i].gamePos = i;
-                games[i].player1 = players[numPlayers];
-                games[i].totalNumPlayers++;
-                break;
+                printf("Paso por for en %d || ", i);
+                printf("%d", games[i].totalNumPlayers);
+                if (games[i].totalNumPlayers == 0)
+                {
+                    players[numPlayers].playerNum = 1;
+                    players[numPlayers].playerPos = numPlayers;
+                    players[numPlayers].gameId = i + 100;
+                    games[i].gameId = i + 100;
+                    games[i].gamePos = i;
+                    games[i].player1 = players[numPlayers];
+                    games[i].totalNumPlayers++;
+                    break;
+                }
+                else if (games[i].totalNumPlayers == 1)
+                {
+                    players[numPlayers].playerNum = 2;
+                    players[numPlayers].playerPos = numPlayers;
+                    players[numPlayers].gameId = i + 100;
+                    games[i].player2 = players[numPlayers];
+                    games[i].totalNumPlayers++;
+                    gamePos = i;
+                    break;
+                }
             }
-            else if (games[i].totalNumPlayers == 1)
-            {
-                players[numPlayers].playerNum = 2;
-                players[numPlayers].playerPos = numPlayers;
-                players[numPlayers].gameId = i + 100;
-                games[i].player2 = players[numPlayers];
-                games[i].totalNumPlayers++;
-                gamePos = i;
-                break;
+            
+            char numPlayerText[2];
+            convertInt2Char(players[numPlayers].playerNum, numPlayerText, sizeof(numPlayerText));
+            printf(" Player num INT  %d", players[numPlayers].playerNum);
+            char gameIdText[4];
+            convertInt2Char(players[numPlayers].gameId, gameIdText, sizeof(gameIdText));
+            strcpy(message, "SERVER GAME_INFO ");
+            concat(message, 7, server_socketStr, " ", numPlayerText, " ", gameIdText, " ", players[numPlayers].addressText);
+            printf("Mensaje =>>   %s   ",message);
+
+            handleCommunication(message);
+            printf("Gamepos ... %d || ", gamePos);
+            if (players[numPlayers].playerNum == 2)
+            {   
+                strcpy(message, "PLAYER SEND_OPP ");
+                concat(message, 5, server_socketStr, " ", games[gamePos].player2.nickname, " ", games[gamePos].player1.addressText);
+                printf("Mensaje =>>   %s   ",message);
+                handleCommunication(message);
+                //sendOpponent(server_socket, games[gamePos].player2.nickname, games[gamePos].player1);
+
+                strcpy(message, "PLAYER SEND_OPP ");
+                concat(message, 5, server_socketStr, " ", games[gamePos].player1.nickname, " ", games[gamePos].player2.addressText);
+                printf("Mensaje =>>   %s   ",message);
+                handleCommunication(message);
+                //sendOpponent(server_socket, games[gamePos].player1.nickname, games[gamePos].player2);
             }
+
+            printf("Player %s connected.\n", players[numPlayers].nickname);
+            ++numPlayers;
         }
+    }else{
+
         
-        char numPlayerText[2];
-        convertInt2Char(players[numPlayers].playerNum, numPlayerText, sizeof(numPlayerText));
-        printf(" Player num INT  %d", players[numPlayers].playerNum);
-        char gameIdText[4];
-        convertInt2Char(players[numPlayers].gameId, gameIdText, sizeof(gameIdText));
-        strcpy(message, "SERVER GAME_INFO ");
-        concat(message, 7, server_socketStr, " ", numPlayerText, " ", gameIdText, " ", players[numPlayers].addressText);
-        printf("Mensaje =>>   %s   ",message);
 
-        handleCommunication(message);
-        printf("Gamepos ... %d || ", gamePos);
-        if (players[numPlayers].playerNum == 2)
-        {   
-            strcpy(message, "PLAYER SEND_OPP ");
-            concat(message, 5, server_socketStr, " ", games[gamePos].player2.nickname, " ", games[gamePos].player1.addressText);
-            printf("Mensaje =>>   %s   ",message);
-            handleCommunication(message);
-            //sendOpponent(server_socket, games[gamePos].player2.nickname, games[gamePos].player1);
-
-            strcpy(message, "PLAYER SEND_OPP ");
-            concat(message, 5, server_socketStr, " ", games[gamePos].player1.nickname, " ", games[gamePos].player2.addressText);
-            printf("Mensaje =>>   %s   ",message);
-            handleCommunication(message);
-            //sendOpponent(server_socket, games[gamePos].player1.nickname, games[gamePos].player2);
-        }
-
-        printf("Player %s connected.\n", players[numPlayers].nickname);
-        ++numPlayers;
     }
     goto fr;
 }
